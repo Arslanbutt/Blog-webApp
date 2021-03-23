a3c083f7caf2e0c3ea6a8ecbc91d1181def09d5d
Arslan <arslanbutt7545@gmail.com>
2021-03-23 11:55:07 +0500

    Dynamic routing enchanced with lodash

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const lodash = require('lodash');
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -17,6 +18,7 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 app.use(express.static("public"));
+
 
 
 
@@ -58,12 +60,19 @@ app.post('/compose', function(req, res) {
 
 
 app.get('/posts/:postName',function(req,res){
-  console.log(req.params.postName);
-
+  //console.log(req.params.postName);
+let urlTitle = lodash.lowerCase(req.params.postName);
 for(post in posts){
   console.log(posts[post].title);
-  if (posts[post].title === req.params.postName){
+  let storedTitle = lodash.lowerCase(posts[post].title);
+  if (urlTitle === storedTitle){
     console.log("Match found");
+    res.render('post',{
+      title : posts[post].title,
+      body : posts[post].body
+    });
+  } else{
+    console.log("Match not found");
   }
 }
 
